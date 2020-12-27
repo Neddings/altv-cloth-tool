b8470bba8c459bfa6fbd787696327481ea015473
Neddings <53919357+Neddings@users.noreply.github.com>
2020-12-27 15:56:01 +0100

    Update ResourceBuilder.cs
    Update to cerulean manifest

--- AltTool/ResourceBuilder.cs
+++ AltTool/ResourceBuilder.cs
@@ -83,7 +83,7 @@ namespace AltTool
             }
 
             string manifestContent = "-- Generated with AltTool\n\n";
-            manifestContent += "fx_version 'bodacious'\n";
+            manifestContent += "fx_version 'cerulean'\n";
             manifestContent += "game { 'gta5' }\n\n";
             manifestContent += $"files {{\n{filesText}\n}}\n\n{metasText}";
             return manifestContent;
